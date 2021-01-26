355e13f2f315ba8452bdf0692577a296010aa4c6
Nicole Dreon <dreon.nicole@gmail.com>
2021-01-26 11:37:51 -0500

    add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// create function
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("The result is " + result);
+}
+
+// call function
+add(4, 5);
+add(2, 4);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
