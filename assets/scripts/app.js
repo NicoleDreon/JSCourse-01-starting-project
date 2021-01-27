c544d631352b2b8ed7da55e4039fb4e7a0b14dc0
Nicole Dreon <dreon.nicole@gmail.com>
2021-01-27 14:43:23 -0500

    refactored code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,26 +1,33 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserInput() {
+function getUserNumberInput() {
   return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
 }
 
 // create add function
 function add() {
   // parseInt() or a + can be used to convert string to number
-  const enteredNumber = getUserInput;
-  const calcDescription = `${currentResult} + ${userInput.value}`;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput("+", initialResult, enteredNumber);
+
   // return ends the function - anyting below, that is within the function, wont be executed
 }
 
 function subtract() {
-  const enteredNumber = getUserInput;
-  const calcDescription = `${currentResult} - ${userInput.value}`;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 // event listener to add button - when add button is clicked have JS looks at the add function and executes it
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
